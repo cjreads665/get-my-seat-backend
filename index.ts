3514d03b5aa714e94c8b43b38667f5bc882acaf7
cjreads665 <shahid.alam57@gmail.com>
2023-03-14 21:25:06 +0545

    added status code to send to client

--- index.ts
+++ index.ts
@@ -10,13 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  // let test = 6116590703; //6715741541 //6116590703 //6316590028
-  //   let k= await ScrapController(test);
-  //   console.log(k);
-  //   // res.json(k)
+
     res.send("Server for seat featching");
 });
 
@@ -27,7 +21,12 @@ app.post("/get-status", async (req: Request, res: Response) => {
   const {pnr} = req.body
   console.log(pnr);
   let result= await ScrapController(pnr);
-  res.json(result)
+  console.log(result);
+  if(result?.status===404){
+    res.sendStatus(404);
+  } else{
+    res.json(result)
+  }
 });
 
 const port = process.env.PORT || 8080;
